8c7145d18d8242f0becc849f4fd9048c1db877ed
Milton Chandradas <milton.chandradas@sap.com>
2026-07-07 14:38:20 -0400

    READ handler

--- srv/service.js
+++ srv/service.js
@@ -4,7 +4,16 @@ module.exports = async (srv) => {
   const remoteService = await cds.connect.to('SALM_TM_TWL_SRV')
 
   srv.on('READ', 'Defects', async req => {
-    req.query.limit(5, 0)
-    return remoteService.run(req.query)
+    const query = { ...req.query, SELECT: { ...req.query.SELECT } }
+
+    delete query.SELECT.limit
+
+    const result = await remoteService.run(query)
+
+    if (!Array.isArray(result)) {
+      return result
+    }
+
+    return result.slice(0, 5)
   })
 }
